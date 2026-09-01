c970a8065c2253404d1d3bd2284e50e287431aca
LeoXu <leoxu037@gamil.com>
2026-09-01 19:43:13 +0800

    dsa binary_search

--- Data Structure algorithms/arry/arry.md
+++ Data Structure algorithms/arry/arry.md
@@ -12,3 +12,28 @@ core idea
 1. 每次检查中间
 2. 排除一半不可能区间
 3. 时间复杂度 log n
+
+### inclusive on both ends VS. left-inclusive and right-exclusive
+1. 右边边界可以取到 故
+```cpp
+    int right = nums.size() - 1; // start with 0
+    while(left <= right ){
+        if(nums[middle] > target){
+            right = middle - 1; //右边界可取的情况下，nums[middle]这个数明显大于right 舍弃
+        }
+    }
+```
+2. 右边界取不到
+```cpp
+    int right = nums.size(); // start with 0
+    while(left < right ){
+        if(nums[middle] > target){
+            right = middle; //右边界不可取的情况下，即使nums[middle]这个数明显大于right，也不会进入下一次loop
+        }
+    }
+```
+### summary 
+1. 二分查找 
+   1. 注意边界值是否取到 三处不同 用边界值判断 右边界是否可取
+   2. 注意nums[middle] 与 target比较
+   3. middle = left + （right - left） /2；非负且防止溢出
--- Data Structure algorithms/arry/binary_search src/binary_search.cpp
+++ Data Structure algorithms/arry/binary_search src/binary_search.cpp
@@ -1,14 +1,14 @@
-#include <iostream> //inclusive on both ends
+#include <iostream> 
 #include <vector>
 using namespace std;
 
-int search (vector<int>& nums, int target){
+int search1 (vector<int>& nums, int target){ //inclusive on both ends
     int left = 0;
-    int right = nums.size() - 1;
-    while(left<=right){
+    int right = nums.size() - 1;//right-inclusive
+    while(left<=right){ //right-inclusive
         int middle = left + (right - left)/2;
         if(nums[middle]>target){
-            right = middle -1;
+            right = middle -1; //right0inclusive
         } else if (nums[middle]<target){
             left = middle + 1;
         } else {
@@ -16,6 +16,27 @@ int search (vector<int>& nums, int target){
         }
     }
     return -1;  
+}
+
+int search2 (vector<int>& nums, int target){ //left-inclusive and right-exclusive
+    int left = 0;
+    int right = nums.size();
+
+    while(left < right){
+        int middle = left + (right - left) / 2;
+
+        if(nums[middle] > target){
+            right = middle;
+        } else if (nums[middle] < target){
+            left = middle + 1;
+        } else {
+            return middle;
+        }
+
+        return -1;
+
+    }
+
 }
 int main(){
     vector<int> v;
@@ -29,8 +50,15 @@ int main(){
     v.push_back(17);
     v.push_back(19);
     
-    int result = search(v, 15);
+    int result = search1(v, 15);
     if (result != -1) {
+    cout << "Target found at index: " << result << endl;
+    } else {
+    cout << "Target not found!" << endl;
+    }
+
+    int result1 = search2(v, 15);
+    if (result1 != -1) {
     cout << "Target found at index: " << result << endl;
     } else {
     cout << "Target not found!" << endl;
